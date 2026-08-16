bf97d1b592bf72e38e1a15ea0f90b7dbae051dc7
Ari-ICU <thoeurn.ratha.kh@gmail.com>
2026-08-16 13:38:09 +0700

    chore: configure GitHub Pages manual basePath and remove automatic static site generator setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? "/tech-academy" : undefined,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   experimental: {
     mdxRs: false,
